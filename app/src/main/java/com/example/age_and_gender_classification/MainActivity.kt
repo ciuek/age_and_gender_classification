0be4122a5ce5b400466ef8cea477e7b23e2b971f
Maciej <mechlinskimaciej@gmail.com>
2022-12-07 01:34:19 +0100

    bitmap convert

--- app/src/main/java/com/example/age_and_gender_classification/MainActivity.kt
+++ app/src/main/java/com/example/age_and_gender_classification/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.age_and_gender_classification
 
 import android.content.pm.PackageManager
+import android.graphics.*
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -13,6 +15,7 @@ import com.example.age_and_gender_classification.databinding.ActivityMainBinding
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.FaceDetection.getClient
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -92,6 +95,7 @@ class MainActivity : AppCompatActivity(), ImageAnalysis.Analyzer  {
             object : ImageCapture.OnImageCapturedCallback() {
                 override fun onCaptureSuccess(image: ImageProxy) {
                     analyze(image)
+                    image.close()
                 }
 
                 override fun onError(exception: ImageCaptureException) {
@@ -160,10 +164,29 @@ class MainActivity : AppCompatActivity(), ImageAnalysis.Analyzer  {
         cameraExecutor.shutdown()
     }
 
-    override fun analyze(image: ImageProxy) {
-        val mediaImage = image.image
+    fun Image.toBitmap(): Bitmap {
+        val yBuffer = planes[0].buffer // Y
+        val vuBuffer = planes[2].buffer // VU
+
+        val ySize = yBuffer.remaining()
+        val vuSize = vuBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + vuSize)
+
+        yBuffer.get(nv21, 0, ySize)
+        vuBuffer.get(nv21, ySize, vuSize)
+
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, this.width, this.height, null)
+        val out = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 50, out)
+        val imageBytes = out.toByteArray()
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    }
+
+    override fun analyze(imageproxy: ImageProxy) {
+        val mediaImage = imageproxy.image
         if (mediaImage != null) {
-            val image = InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees)
+            val image = InputImage.fromMediaImage(mediaImage, imageproxy.imageInfo.rotationDegrees)
             val options = FaceDetectorOptions.Builder()
                 .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
                 .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
@@ -176,19 +199,43 @@ class MainActivity : AppCompatActivity(), ImageAnalysis.Analyzer  {
 
             val result = detector.process(image)
                 .addOnSuccessListener { faces ->
+                    if(faces.size == 0)
+                    {
+                        Toast.makeText(this,
+                            "nie ma rocka :(",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                    val bitmap_img = mediaImage.toBitmap()
                     for (face in faces) {
                         Toast.makeText(this,
                             "WYKRYTO ROCKA!!",
                             Toast.LENGTH_SHORT).show()
                         val bounds = face.boundingBox
-                        val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
-                        val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
+
+                        val x = Math.max(bounds.left, 0)
+                        val y = Math.max(bounds.top, 0)
+
+                        val width = bounds.width()
+                        val height = bounds.height()
+//                        val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
+//                        val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
                         bounds.set(
                             bounds.left,
                             bounds.top,
                             bounds.right,
                             bounds.bottom
                         )
+
+                        val crop = Bitmap.createBitmap(
+                            bitmap_img,
+                            x,
+                            y,
+                            if(x + width > bitmap_img.width) bitmap_img.width - x else width,
+                            if(y + width > bitmap_img.height) bitmap_img.height - x else height
+                        )
+
+                        croppedIv.setImageBitmap
+
 
                     }
                 }
